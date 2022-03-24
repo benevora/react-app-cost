04adc973720b7ce913df309e2adb692d0bcd0cb0
Ben Evora <65423996+benevora@users.noreply.github.com>
2022-03-24 12:13:26 -0400

    creating the home page

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,6 +1,18 @@
+import Styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
+
 function Home() {
  
-  return <h1>Home</h1>
+  return (
+    <section className={Styles.home_container}>
+      <h1>Ben-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar Projeto" />
+      <img src={savings} alt="Costs"/>
+    </section>
+  )
 }
  
 export default Home
