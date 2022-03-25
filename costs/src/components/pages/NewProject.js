8a2f52341f89be4fd4714f259f2ee764584653b5
Ben Evora <65423996+benevora@users.noreply.github.com>
2022-03-25 11:12:10 -0400

    form componentization

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,6 +1,15 @@
+import styles from './NewProject.module.css';
+import ProjectForm from '../project/ProjectForm'
+
 function NewProject() {
  
-  return <h1>NewProject</h1>
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os servicos</p>
+      <ProjectForm btnText="Criar Projeto" />
+    </div>
+  )
 }
  
 export default NewProject
