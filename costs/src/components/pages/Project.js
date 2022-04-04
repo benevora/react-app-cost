4299532b5efcfdc755bd8e0bb9241977d000b118
Ben Evora <65423996+benevora@users.noreply.github.com>
2022-04-04 13:36:08 -0400

    Showing the services

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -11,12 +11,14 @@ import Container from '../layout/Container'
 import ProjectForm from '../project/ProjectForm'
 import Message from '../layout/Message'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
 
 function Project() {
  
   const {id} = useParams()
 
   const [project, setProject] = useState([])
+  const [services, setServices] = useState([])
   const [showProjectForm,setShowProjectForm] = useState(false)
   const [showServiceForm,setShowServiceForm] = useState(false)
   const [message, setMessage] = useState()
@@ -33,6 +35,7 @@ function Project() {
       .then((resp) => resp.json())
       .then((data) => {
         setProject(data)
+        setServices(data.services)
       })
       .catch((err) => console.log(err))
     }, 300)
@@ -99,11 +102,14 @@ function Project() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        // exibir os servicos
-        console.log(data)
+        setShowServiceForm(false)
       })
       .catch((err) => console.log(err))
 
+  }
+
+  function removeService() {
+    
   }
 
   function toggleProjectForm() {
@@ -166,7 +172,19 @@ function Project() {
             </div>
             <h2>Servicos</h2>
             <Container customClass="start">
-              <p>Itens de servicos</p>
+              {services.length > 0 &&
+                services.map((service) => (
+                  <ServiceCard 
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.id}
+                    handleRemove={removeService}
+                  />
+                ))
+              }
+              {services.length === 0 && <p>Nao ha servicos cadastrados.</p> }
             </Container>
           </Container>
         </div>
